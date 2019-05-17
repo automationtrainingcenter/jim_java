3afb403ec6af4865824bd1f00c6909dc6cbe9dba
automation <atcsurya@gmail.com>
2019-05-17 09:23:25 +0530

    variables intro

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -1,11 +1,8 @@
 package basics;
 
 public class HelloWorld {
-	
 	public static void main(String[] args) {
 		System.out.println("Hello wolrd");
 		System.out.print("welcome to Java Programming course");
 	}
-	
-
 }
---
+++ src/basics/VariablesDemo.java
@@ -0,0 +1,77 @@
+package basics;
+
+/*
+* Variable is name of memory location where we store data. 
+*
+* if we want to make any variable as constant then use "final" keyword.
+* 
+* Types of variables
+* 
+* 1.Instance variables 
+* 		It is a variable which is declared inside the class but outside of any method. 
+* These are object level. We can access these variables directly inside the class 
+* instance methods and by creating an object of the class in same class static methods 
+* and outside the class. 
+* Variable declaration
+* 		access_modifier data_type var_name; 
+* accessing variable 
+* 		object_name.var_name = value; 
+* Initialization variable: At the time of variable declaration we are going to assign some value 
+* 		access_modifier data_type var_name = value;
+* 
+* 2.static variables 
+* 		It is a variable which declared inside the class and outside of any method 
+* but declared using 'static' keyword. These are class level i.e. object to object these 
+* variables will share the information. We can access these variables directly inside the 
+* class and by using class_name outside the class.
+* Variable declaration 
+* 		access_modifier static data_type var_name; 
+* accessing variable 
+* 		class_name.var_name = value; 
+* Initialization
+* 		access_modifier static data_type var_name = value;
+* 
+* 3.local variables 
+* 		It is variable which is declared inside any method. we can
+* access this variable inside the method only. 
+* Variable declaration 
+* 		data_type var_name; 
+* accessing variable 
+* 		var_name = value; 
+* Initialization 
+* data_type var_name = value;
+* 
+*/   
+
+
+public class VariablesDemo {
+	// instance
+	int iVar = 10;
+	//static 
+	static int sVar = 20;
+
+	//instance method
+	public void iMethod() {
+		//accessing instance variable
+		iVar = 20;
+		//accessing static variable
+		sVar = 40; 
+	}
+	
+	public static void main(String[] args) {
+		//local
+		int lVar = 30;
+		System.out.println(lVar);
+		//create an object of the VariblesDemo class
+		//class_name obj_name = new class_name();
+		VariablesDemo obj = new VariablesDemo();
+		//accessing instance variable
+		System.out.println(obj.iVar);
+		//accessing static variable
+		System.out.println(sVar);
+		
+		
+		
+	}
+
+}
